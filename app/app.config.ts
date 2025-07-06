308338b45b834ed445d7d669f1e6eec9b356e6e7
oaaalk <www.oaaa.lk@gmail.com>
2025-07-06 12:47:27 +0530

    5 changes by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,19 +1,7 @@
 export default defineAppConfig({
-  global: {
-    picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'My profile picture'
-    },
-    meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
-    available: true
-  },
+  global: 'www.oaaa.lk@gmail.com',
   ui: {
-    colors: {
-      primary: 'blue',
-      neutral: 'neutral'
-    }
+    colors: 'stone'
   },
   uiPro: {
     pageHero: {
