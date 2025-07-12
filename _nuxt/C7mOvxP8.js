2aba3bebedd95fd5127435d2ec8e015291cba43e
sankhaKarunasekara <sankhadlk@gmail.com>
2025-07-12 01:35:14 +0000

    Deploying to gh-pages from @ oaaalk/www.oaaa.lk@a6255744501d8ce38eb07035bf5a5883be5a3ba0 🚀

---
+++ _nuxt/C7mOvxP8.js
@@ -0,0 +1,1 @@
+import{r as n}from"./BO0uabfV.js";import a from"./DwjI5hdv.js";import{y as s,B as t,h as e,w as i,E as p}from"./DLfRHnOu.js";import"./BcoM21Dg.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
